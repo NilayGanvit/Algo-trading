bc93ee3e24d5203b840139f34f02ec1cc42179b2
NilayGanvit <79703214+NilayGanvit@users.noreply.github.com>
2022-08-04 00:19:24 +0530

    StockScreener
    added stock screener and telegram alert

---
+++ StockScreener/StockScreener-main/3TightRangeSorting.py
@@ -0,0 +1,29 @@
+import time
+import os
+import glob
+import pandas as pd
+from datetime import datetime
+import numpy as np
+
+dict = {}
+stocks = pd.read_csv('stocklist.csv')
+df = pd.DataFrame(stocks)
+df = df.iloc[: , 1:]
+arr = df["0"].to_numpy()
+y = 2
+# print(arr)
+for stock in arr:
+
+    df = pd.read_csv(f'filesForToday/{stock}.csv')
+    df = pd.DataFrame(df)
+    
+    minhigh = df.iloc[0+y]['h']
+    minlow = df.iloc[0+y]['l']
+    rangee = (minhigh-minlow)/minhigh*100
+    stock = stock+","
+    dict[stock] = rangee
+
+a = sorted(dict.items(), key=lambda x: x[1])
+# print(a) 
+a = pd.DataFrame(a)
+a.to_csv('stocksToTrade.csv')  
